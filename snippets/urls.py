d5d859f314b5dc96c357b7d938b3bdcb704a8aaf
jseop-lim <jseoplim@gmail.com>
2022-02-06 01:31:50 +0900

    [T2] refactor: snippets views with flexible request and response type

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 app_name = 'snippets'
 
 urlpatterns = [
-    path('', views.snippet_list, name='list'),
-    path('<int:pk>/', views.snippet_detail, name='detail'),
+    path('snippets', views.snippet_list, name='list'),
+    path('snippets/<int:pk>', views.snippet_detail, name='detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
