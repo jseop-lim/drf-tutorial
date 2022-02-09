ecdf7a6cef832f5339746d29a7ca34a668e59922
jseop-lim <jseoplim@gmail.com>
2022-02-09 19:57:55 +0900

    [T5] feat: Hyperlinking API

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,24 +2,27 @@ from rest_framework import serializers
 from snippets.models import Snippet
 from django.contrib.auth.models import User
 
-class SnippetSerializer(serializers.ModelSerializer):
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     # model field를 오버라이딩, GET 요청에만 이용됨
     owner = serializers.ReadOnlyField(source='owner.username')
-    
+    # model에 없는 field이므로 instance 생성 시 data로 전달받을 필요 없음
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
 
 
-class UserSerializer(serializers.ModelSerializer):
-    # snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
-    snippets = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    password = serializers.CharField(write_only = True)
-    
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    password = serializers.CharField(write_only = True)  # added
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'snippets']
-        
+        fields = ['url', 'id', 'username', 'password', 'snippets']
+
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
